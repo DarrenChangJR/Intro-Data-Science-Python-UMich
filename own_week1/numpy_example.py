a25b430a3d60e68469629476f4d4b9f4b9a8238d
DarrenChangJR <darrenjr@connect.hku.hk>
2022-03-07 16:25:25 +0800

    Initial commit

---
+++ own_week1/numpy_example.py
@@ -0,0 +1,44 @@
+import numpy as np
+import math
+
+# a = np.array([[1,2,3], [4,5,6]])
+# print(a, a.ndim)
+# print(a.shape, a.dtype)
+
+# b = np.zeros((2,3))
+# b = np.ones((3,2))
+# b = np.random.rand(2,3)
+# b = np.full((2,3), 20)
+# print(b)
+
+# c = np.arange(10,20,3)
+# c = np.linspace(10,20,3)
+# print(c)
+
+
+# ELEMENT-WISE OPERATIONS
+farenheit = np.array([0,-10,-5,-15,0])
+celcius = (farenheit - 32) * (5/9)
+print(celcius)
+print(celcius > -20)
+
+a = np.arange(1,7).reshape(2,3)
+b = np.array([[1,2,3], [4,5,6]])
+print(a*b)
+
+# MATRIX-WISE OPERATIONS
+print(a.transpose()@b)
+print(a@b.transpose())
+
+# AGGREGATION
+print(a.sum(), a.max(), a.min(), a.mean())
+
+
+# INDEXING
+a = np.arange(1,28).reshape(3,3,3)
+print(a, a[1,0])
+print(a[[0,1,2], [0,1,1]])
+
+print(a[a>10])
+
+# WITH DATASETS
